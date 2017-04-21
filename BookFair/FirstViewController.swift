87499afca61ed4a5fe524051269e34c323cd58ff
Aref Abedjooy <arefabedjooy@gmail.com>
2017-04-21 23:27:18 +0430

    Initial Commit

---
+++ BookFair/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BookFair
+//
+//  Created by Aref Abedjooy on 4/21/17.
+//  Copyright © 2017 Aref Abedjooy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BookFair/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BookFair
+//
+//  Created by Aref Abedjooy on 4/21/17.
+//  Copyright © 2017 Aref Abedjooy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
